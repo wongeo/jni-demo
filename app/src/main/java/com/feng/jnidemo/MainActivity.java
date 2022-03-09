af961b33cb043d62c66f036d6e0eaa8df74c5b38
wongeo <29992379@qq.com>
2022-03-09 21:13:59 +0800

    update

--- app/src/main/java/com/feng/jnidemo/MainActivity.java
+++ app/src/main/java/com/feng/jnidemo/MainActivity.java
@@ -1,10 +1,17 @@
 package com.feng.jnidemo;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.feng.cpplib.JNI;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +28,17 @@ public class MainActivity extends AppCompatActivity {
         JNI jni = new JNI();
         String str = jni.stringFromJNI();
 
+        Bitmap bitmap = loadBitmap();
+
+        bitmap = processBitmap(bitmap);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        imageView.setImageBitmap(bitmap);
+
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-
-
+        tv.setText(str);
     }
 
     /**
@@ -33,4 +46,37 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+
+    /**
+     * 从assets中加载图片
+     */
+    private Bitmap loadBitmap() {
+        Bitmap bmp = null;
+        AssetManager am = getResources().getAssets();
+        try {
+            InputStream is = am.open("dog.jpg");
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            bmp = BitmapFactory.decodeStream(is, null, options);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmp;
+    }
+
+    /**
+     * 处理图片，此方法中会调用nativeProcessBitmap
+     *
+     * @param bitmap
+     * @return
+     */
+    private Bitmap processBitmap(Bitmap bitmap) {
+        Bitmap bmp = null;
+        if (bitmap != null) {
+            bmp = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+            new JNI().nativeProcessBitmap(bmp);
+        }
+        return bmp;
+    }
 }
